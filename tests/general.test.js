32dae078b8153c465c7b729508fb5f86f63c6315
root <root@localhost>
2022-03-06 19:32:29 +0100

    add tests

---
+++ tests/general.test.js
@@ -0,0 +1,26 @@
+const reader = require('../cjs/index').default;
+
+test('it can use @static-pages/file-reader', async () => {
+	const expected = ['file1', 'file2', 'file3'];
+
+	const output = [...reader({
+		cwd: 'tests/input',
+		pattern: '**/file*.yaml',
+	})].map(x => x.a);
+
+	output.sort((a, b) => a.localeCompare(b));
+	expect(output).toStrictEqual(expected);
+});
+
+test('attrKey can be customized', async () => {
+	const expected = ['file1', 'file2', 'file3'];
+
+	const output = [...reader({
+		cwd: 'tests/input',
+		pattern: '**/file*.yaml',
+		attrKey: 'attr',
+	})].map(x => x.attr.a);
+
+	output.sort((a, b) => a.localeCompare(b));
+	expect(output).toStrictEqual(expected);
+});
